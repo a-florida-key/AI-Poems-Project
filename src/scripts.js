6cea6e8d9bab89e7ef935f86c9207250668ca424
a-florida-key <a.florida.key@gmail.com>
2023-12-30 15:11:11 -0500

    added basic JS

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,21 @@
-alert("Hey you!");
+function Poem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 50,
+  });
+}
+
+function getPoem(event) {
+  event.preventDefault();
+  let prompt = ``;
+  let context = `You are a poem AI assistant. Please provide answers that ryhme.`;
+  let apiKey = `ceao63d7e80ab9169tc53ef7497c0dda`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(Poem);
+}
+
+letpoemFormElement = document.querySelector("poem-generator");
+poemFormElement.addEventListener("submit", getPoem);
