108f49aebaa4eeb6ef83f1d6dc0c6877ab3386be
a-florida-key <a.florida.key@gmail.com>
2023-12-30 16:58:33 -0500

    revised AI, adding animation

--- src/scripts.js
+++ src/scripts.js
@@ -11,9 +11,13 @@ function Poem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
   let prompt = `Generate a poem about ${instructionsInput.value}`;
-  let context = `You are a poem AI assistant. Please provide a short poem in basic HTML. Please make sure the poem rhymes. Please sign the poem with '- SheCodes AI' inside a <strong> element.`;
+  let context = `You are a poem AI assistant. Please provide a short, concise poem using basic HTML with each line seperated by a <br />. Please make sure the poem rhymes. Please sign the poem '- SheCodes AI' at the end of the poem, inside a <strong> element.`;
   let apiKey = `ceao63d7e80ab9169tc53ef7497c0dda`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating the poem about "${instructionsInput.value}"...⌛</div>`;
 
   axios.get(apiUrl).then(getPoem);
 }
